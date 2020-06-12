39d5ca7e66c2f0c676c1ad3f4d2b890d9b05d52e
ujihisa <ujihisa@users.noreply.github.com>
2020-06-11 17:09:04 -0700

    app_mention

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -9,6 +9,14 @@ class SlackController < ApplicationControllerApi
     case req['type']
     when 'url_verification'
       render plain: req['challenge']
+    when 'event_callback'
+      case req['event']['type']
+      when 'app_mention'
+        Rails.logger.info("app_mention #{JSON.pretty_generate(req)}"
+        render plain: { ok: true }
+      else
+        raise "What's this req: #{JSON.pretty_generate(req)}"
+      end
     else
       raise "What's this req: #{JSON.pretty_generate(req)}"
     end
