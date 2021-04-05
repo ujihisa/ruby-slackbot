caab5bdc1207b78c6f379670a7b5226358d6a2fa
ujihisa <ujihisa@users.noreply.github.com>
2021-04-05 13:49:08 -0700

    Introduce ShuffledQueue

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,3 +1,30 @@
+class ShuffledQueue
+  def initialize(xs)
+    @orig_xs = xs.freeze
+    reset
+  end
+
+  def pop
+    @xs.pop
+  end
+
+  def reset
+    @xs = @orig_xs.shuffle
+  end
+
+  def inspect
+    <<~EOS
+    Use this like
+      @sq = ShuffledQueue.new(%w[ruby vim].product(%w[clojure scala]))
+      @sq.pop #=> ['vim', 'clojure']
+
+    * ShuffledQueue.new([...])
+    * ShuffledQueue#pop()
+    * ShuffledQueue#reset()
+    EOS
+  end
+end
+
 class SlackController < ActionController::API
   def initialize
     super
