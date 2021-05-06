eef838a1e32ab2285511c05d4a6024f5d6077737
ujihisa <ujihisa@users.noreply.github.com>
2021-05-06 16:19:25 -0700

    Use Bootstrap

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module RubySlackbot
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    APP_STARTED_AT = Time.current
   end
 end
