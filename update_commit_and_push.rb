52034cd2a22dd4b306314e4032c3050eb9fcb0d1
ujihisa <ujihisa@users.noreply.github.com>
2021-07-07 14:10:25 -0700

    Import it form generic-factory-game

---
+++ update_commit_and_push.rb
@@ -0,0 +1,23 @@
+if `git branch --show-current`.chomp != 'master'
+  abort 'Run in master branch.'
+end
+
+system('git pull --rebase', exception: true)
+system('bundle lock --update', exception: true)
+system('doo -b bundle update', exception: true)
+if `git diff -- Gemfile.lock` != ''
+  system("git add Gemfile.lock && git commit -m 'bundle lock --update'", exception: true)
+end
+
+system('doo yarn upgrade -s', exception: true)
+if `git diff -- yarn.lock` != ''
+  system("git add ./yarn.lock && git commit -m 'yarn upgrade'", exception: true)
+end
+
+system('doo -b bin/rails test', exception: true)
+
+system('git push', exception: true)
+
+puts 'All done'
+
+system('docker-compose down', exception: true)
