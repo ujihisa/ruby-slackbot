70beb618e101449192e53ad18e709bdecd4e8e49
ujihisa <ujihisa@users.noreply.github.com>
2023-03-23 15:17:02 -0700

    Use httpclient instead

--- app/controllers/fake_slack_controller.rb
+++ app/controllers/fake_slack_controller.rb
@@ -5,18 +5,19 @@ class FakeSlackController < ApplicationController
   end
 
   def post
-    r, w = IO.pipe
-    system('curl', '-s', '-d', {
-      type: 'event_callback',
-      event: {
-        type: 'app_mention',
-        text: "<@a> #{params[:body]}",
-        channel: 'CPJDWPTJA',
+    r = HTTPClient.new(header: 'Content-type:application/json').post(
+      'http://localhost:3000/slack/api',
+      {
+        type: 'event_callback',
+        event: {
+          type: 'app_mention',
+          text: "<@a> #{params[:body]}",
+          channel: 'CPJDWPTJA',
+        }
       }
-    }.to_json, 'http://localhost:3000/slack/api', out: w)
-    w.close
+    )
 
-    flash[:reply] = JSON.parse(r.read)['posted_to_slack']
+    flash[:reply] = JSON.parse(r.body)['posted_to_slack']
     respond_to do |format|
       format.html { redirect_to action: :get  }
     end
