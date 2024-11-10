f7e2e3975734563ce5cc641628898bd01dfa9cda
ujihisa <ujihisa@users.noreply.github.com>
2024-11-10 14:09:41 -0800

    Why did it change?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  config.hosts << 'ruby-slackbot-jp-t7ero6olsa-an.a.run.app'
+  config.hosts << 'ruby-slackbot-jp-157700815359.asia-northeast1.run.app'
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
