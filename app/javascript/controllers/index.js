13156037173132d66f39a413cc99489ad07c49b2
ujihisa <ujihisa@users.noreply.github.com>
2024-11-11 13:45:51 -0800

    Remove all unused files

--- app/javascript/controllers/index.js
+++
@@ -1,5 +0,0 @@
-// Import and register all your controllers from the importmap under controllers/*
-
-import { application } from "controllers/application"
-import { registerControllersFrom } from "@hotwired/stimulus-importmap-autoloader"
-registerControllersFrom("controllers", application)
