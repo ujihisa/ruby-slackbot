92dfec291579d123716d65d9692a8d6b4321e29b
ujihisa <ujihisa@users.noreply.github.com>
2025-03-10 17:22:49 -0700

    Let's try again

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -42,9 +42,8 @@ class SlackController < ActionController::API
           render json: { ok: true }
         in text
           if /\b(sample|send|public_send)\b/ =~ text && channel == 'C015ZM53B40'
-            if false
-              # formatted_result = "今週は#{$1}()以外を使ってみようキャンペーン実施中です:fufufu:\nhttps://rurema.clear-code.com/3.2.0/method/Array/i/#{$1}.html"
-              formatted_result = ":fufufu:\nもう一度試すと、うまくいくかも?"
+            if true
+              formatted_result = "今週は#{$1}()以外を使ってみようキャンペーン実施中です:fufufu:\nhttps://rurema.clear-code.com/3.2.0/method/Array/i/#{$1}.html"
               post_slack(channel, formatted_result)
               render json: { ok: true, posted_to_slack: formatted_result }
               return
